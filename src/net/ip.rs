446f853530a5580a30684c7e658fe9df0295cde5
Luo Jia <me@luojia.cc>
2021-10-25 18:15:36 +0800

    IP subnet

--- src/net/ip.rs
+++ src/net/ip.rs
@@ -7,41 +7,57 @@ use core::fmt;
 // Ipv6 address
 #[derive(Clone, Copy, PartialEq, Eq)]
 pub struct Address {
-    segments: [u16; 8],
+    repr: u128,
 }
 
 impl Address {
+    pub const UNSPECIFIED: Self = Address::from_segments([0, 0, 0, 0, 0, 0, 0, 0]);
+    pub const LOOPBACK: Self = Address::from_segments([0, 0, 0, 0, 0, 0, 0, 1]);
+    pub const fn from_segments(segments: [u16; 8]) -> Self {
+        let repr = ((segments[0] as u128) << 112) | ((segments[1] as u128) << 96) |
+            ((segments[2] as u128) << 80) | ((segments[3] as u128) << 64) |
+            ((segments[4] as u128) << 48) | ((segments[5] as u128) << 32) |
+            ((segments[6] as u128) << 16) | (segments[7] as u128);
+        Self { repr }
+    }
     pub fn from_bytes(src: &[u8]) -> Self {
-        assert!(src.len() == 16);
-        let mut segments = [0u16; 8];
-        for idx in 0..8 {
-            let buf = &src[idx * 2..];
-            let segment = NetworkEndian::read_u16(buf);
-            segments[idx] = segment;
-        }
-        Address { segments }
+        let repr = NetworkEndian::read_u128(&src);
+        Address { repr }
+    }
+    pub const fn segments(self) -> [u16; 8] {
+        let repr = self.repr;
+        [
+            ((repr >> 112) & 0xFFFF) as u16, ((repr >> 96) & 0xFFFF) as u16,
+            ((repr >> 80) & 0xFFFF) as u16, ((repr >> 64) & 0xFFFF) as u16,
+            ((repr >> 48) & 0xFFFF) as u16, ((repr >> 32) & 0xFFFF) as u16,
+            ((repr >> 16) & 0xFFFF) as u16, (repr & 0xFFFF) as u16,
+        ]
+    }
+    pub const fn octets(self) -> [u8; 16] {
+        self.repr.to_be_bytes()
     }
 }
 
 impl From<[u8; 16]> for Address {
-    fn from(octets: [u8; 16]) -> Address {
+    #[inline] fn from(octets: [u8; 16]) -> Address {
         Address::from_bytes(&octets)
     }
 }
 
 impl From<[u16; 8]> for Address {
-    fn from(segments: [u16; 8]) -> Address {
-        Address { segments }
+    #[inline] fn from(segments: [u16; 8]) -> Address {
+        Address::from_segments(segments)
     }
 }
 
 impl fmt::Display for Address {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let segments = self.segments();
         let (longest_idx, longest_len) = {
             let (mut longest_idx, mut longest_len) = (0, 0);
             let mut cur_len = 0;
             for idx in 0..8 {
-                if self.segments[idx] == 0 {
+                if segments[idx] == 0 {
                     cur_len += 1;
                     if cur_len > longest_len {
                         longest_len = cur_len;
@@ -59,7 +75,7 @@ impl fmt::Display for Address {
                 write!(f, "::")?;
                 idx += longest_len;
             } else {
-                write!(f, "{:X}", self.segments[idx])?;
+                write!(f, "{:X}", segments[idx])?;
                 idx += 1;
                 if idx != 8 && idx != longest_idx {
                     write!(f, ":")?;
@@ -154,16 +170,45 @@ impl From<Protocol> for u8 {
     }
 }
 
-/*
+// for example: FE80::/10
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub struct Subnet {
-    network: Address,
+    network: Address, // any bits beyond the prefix should be 0
     prefix: u8,
 }
-*/
+
+impl Subnet {
+    // check prefix mask
+    pub const fn new(network: Address, prefix: u8) -> Subnet {
+        assert!(prefix <= 128);
+        assert!(network.repr.trailing_zeros() >= 128 - prefix as u32);
+        unsafe { Self::new_unchecked(network, prefix) }
+    }
+    // variable 'network' must have last '128 - prefix' bits set to zero
+    // this is not checked here so it's unsafe
+    pub const unsafe fn new_unchecked(network: Address, prefix: u8) -> Self {
+        Self { network, prefix }
+    }
+    // Section 2.4, https://datatracker.ietf.org/doc/html/rfc4291
+    pub const UNSPECIFIED: Subnet =
+        Subnet { network: Address::from_segments([0, 0, 0, 0, 0, 0, 0, 0]), prefix: 128 };
+    pub const LOOPBACK: Subnet =
+        Subnet { network: Address::from_segments([0, 0, 0, 0, 0, 0, 0, 1]), prefix: 128 };
+    pub const MULTICAST: Subnet =
+        Subnet { network: Address::from_segments([0xff00, 0, 0, 0, 0, 0, 0, 0]), prefix: 8 };
+    pub const LINK_LOCAL_UNICAST: Subnet =
+        Subnet { network: Address::from_segments([0xfe80, 0, 0, 0, 0, 0, 0, 0]), prefix: 10 };
+}
+
+impl fmt::Display for Subnet {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}/{}", self.network, self.prefix)
+    }
+}
 
 #[cfg(test)]
 mod tests {
-    use super::Address;
+    use super::{Address, Subnet};
     #[test]
     fn ip_address_print() {
         assert_eq!("::", Address::from([0, 0, 0, 0, 0, 0, 0, 0]).to_string());
@@ -172,5 +217,23 @@ mod tests {
         assert_eq!("FF01::101", Address::from([0xff01, 0, 0, 0, 0, 0, 0, 0x101]).to_string());
         assert_eq!("2001:DB8::8:800:200C:417A", Address::from([0x2001, 0xdb8, 0, 0, 8, 0x800, 0x200c, 0x417a]).to_string());
         assert_eq!("2001:DB8:0:CD30::", Address::from([0x2001, 0xdb8, 0, 0xcd30, 0, 0, 0, 0]).to_string());
+    }
+    #[test]
+    fn ip_address_segments_octets() {
+        assert_eq!(
+            Address::from([0xfe80, 0, 0, 0, 0, 0, 0x1234, 0x5678]).segments(), 
+            [0xfe80, 0, 0, 0, 0, 0, 0x1234, 0x5678]
+        );
+        assert_eq!(
+            Address::from([0xfe80, 0, 0, 0, 0, 0, 0x1234, 0x5678]).octets(), 
+            [0xfe, 0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x12, 0x34, 0x56, 0x78]
+        );
+    }
+    #[test]
+    fn ip_subnet_print() {
+        assert_eq!("FF00::/8", Subnet::MULTICAST.to_string());
+        assert_eq!("FE80::/10", Subnet::LINK_LOCAL_UNICAST.to_string());
+        assert_eq!("::1/128", Subnet::LOOPBACK.to_string());
+        assert_eq!("::/128", Subnet::UNSPECIFIED.to_string());
     }
 }
